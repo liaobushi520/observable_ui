1a4efc0baa0a6c5acab2e62ecc2312d978c6f2a1
zhongjun.liao <qiaofang123>
2019-08-30 15:35:34 +0800

    Initial commit

---
+++ lib/observable_ui.dart
@@ -0,0 +1,4 @@
+library observable_ui;
+
+export 'src/core.dart';
+export 'src/widgets.dart';
---
+++ test/observable_ui_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:observable_ui/observable_ui.dart';
+
+void main() {}
